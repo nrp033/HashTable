17b49a3d2419d9c1a4aa9979e9e59368ffe8b5db
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-19 16:02:35 +0530

    Creating Method To Get Frequency of Words From Sentence

---
+++ HashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            hash.GetFrequency("To");
+            hash.GetFrequency("be");
+            hash.GetFrequency("or");
+            hash.GetFrequency("not");
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
